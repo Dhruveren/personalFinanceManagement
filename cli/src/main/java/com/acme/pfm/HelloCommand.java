f406e67dc43fc90055847aaef6d2851e1f92bfbb
Debasish Sarkar <trader2004jpg@gmail.com>
2025-09-02 17:45:18 +0530

    chore(build): maven multi-module + deps

--- cli/src/main/java/com/acme/pfm/HelloCommand.java
+++ cli/src/main/java/com/acme/pfm/HelloCommand.java
@@ -1,14 +1,18 @@
 package com.acme.pfm;
-
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "pfm", mixinStandardHelpOptions = true, version = "0.1.0", description = "Says hello")
 
 public class HelloCommand implements Runnable {
+    private static final Logger log = LoggerFactory.getLogger(HelloCommand.class);
+
     public void run() {
+        log.info("HelloCommand started");
         System.out.println("Hello World!");
     }
+
 
     public static void main(String[] args) {
         picocli.CommandLine.run(new HelloCommand(), args);
